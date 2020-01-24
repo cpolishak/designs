f926a01a606183d3e9e492c4e2dddf828cb50139
Chris Polishak <chris@spectre.lan1>
2020-01-24 14:04:21 -0600

    Updates

---
+++ precache-manifest.fff6e4eb56ad80b85ee5cd8a031b88c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f60b5b6902694dd0a7277f9a28317f",
+    "url": "/designs/index.html"
+  },
+  {
+    "revision": "57a6625973dd8738e51e",
+    "url": "/designs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1acc01cbefea51fad793",
+    "url": "/designs/static/js/2.180a45d5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/designs/static/js/2.180a45d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57a6625973dd8738e51e",
+    "url": "/designs/static/js/main.21c66ece.chunk.js"
+  },
+  {
+    "revision": "8061807993b114ab1a9a",
+    "url": "/designs/static/js/runtime-main.626f06b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/designs/static/media/logo.5d5d9eef.svg"
+  }
+]);
